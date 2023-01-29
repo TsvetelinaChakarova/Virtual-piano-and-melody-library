3141bf06690337a2644c4d21791f1cb0b3253873
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-01-29 12:52:12 +0200

    Add piano image as a button

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ app.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 app.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 app.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
+#set piano as button
+piano_image = Image.open("piano.jpg")
+piano_image = piano_image.resize((500, 300))
+piano_tk_image = ImageTk.PhotoImage(piano_image)
+piano_button = tk.Button(app)
+piano_button.pack()
+piano_button.config(image = piano_tk_image)
+
 app.mainloop()
