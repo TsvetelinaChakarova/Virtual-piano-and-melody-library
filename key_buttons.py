56f4ee87d51c1d830d20d0ecfacddac62e46e856
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-01-30 19:09:32 +0200

    Create separate file for key buttons

---
+++ key_buttons.py
@@ -0,0 +1,99 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import play_sound
+
+BLACK_PIANO_KEY_WIDTH = 30
+BLACK_PIANO_KEY_HEIGHT = 140
+WHITE_PIANO_KEY_WIDTH = 60
+WHITE_PIANO_KEY_HEIGHT = 200
+
+def create_key_buttons(app, WINDOW_HEIGHT, size_by_pixel):
+
+    #set white piano keys as buttons
+    C3_key = tk.Button(app, text="C3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_C3)  
+    C3_key.place(x=WHITE_PIANO_KEY_WIDTH*0, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+
+    D3_key = tk.Button(app, text="D3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_D3)  
+    D3_key.place(x=WHITE_PIANO_KEY_WIDTH*1, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)   
+
+    E3_key = tk.Button(app, text="E3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_E3)  
+    E3_key.place(x=WHITE_PIANO_KEY_WIDTH*2, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    F3_key = tk.Button(app, text="F3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_F3)  
+    F3_key.place(x=WHITE_PIANO_KEY_WIDTH*3, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    G3_key = tk.Button(app, text="G3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_G3)  
+    G3_key.place(x=WHITE_PIANO_KEY_WIDTH*4, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    A3_key = tk.Button(app, text="A3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_A3)  
+    A3_key.place(x=WHITE_PIANO_KEY_WIDTH*5, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    B3_key = tk.Button(app, text="B3", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_B3)  
+    B3_key.place(x=WHITE_PIANO_KEY_WIDTH*6, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    C4_key = tk.Button(app, text="C4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_C4)  
+    C4_key.place(x=WHITE_PIANO_KEY_WIDTH*7, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    D4_key = tk.Button(app, text="D4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_D4)  
+    D4_key.place(x=WHITE_PIANO_KEY_WIDTH*8, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    E4_key = tk.Button(app, text="E4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_E4)  
+    E4_key.place(x=WHITE_PIANO_KEY_WIDTH*9, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    F4_key = tk.Button(app, text="F4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_F4)  
+    F4_key.place(x=WHITE_PIANO_KEY_WIDTH*10, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    G4_key = tk.Button(app, text="G4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_G4)  
+    G4_key.place(x=WHITE_PIANO_KEY_WIDTH*11, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    A4_key = tk.Button(app, text="A4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_A4)  
+    A4_key.place(x=WHITE_PIANO_KEY_WIDTH*12, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    B4_key = tk.Button(app, text="B4", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_B4)  
+    B4_key.place(x=WHITE_PIANO_KEY_WIDTH*13, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    C5_key = tk.Button(app, text="C5", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_C5)  
+    C5_key.place(x=WHITE_PIANO_KEY_WIDTH*14, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    D5_key = tk.Button(app, text="D5", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_D5)  
+    D5_key.place(x=WHITE_PIANO_KEY_WIDTH*15, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    E5_key = tk.Button(app, text="E5", fg="#000", image=size_by_pixel, bg='#FFF', height=WHITE_PIANO_KEY_HEIGHT, width=WHITE_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_E5)  
+    E5_key.place(x=WHITE_PIANO_KEY_WIDTH*16, y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT) 
+
+    #set black piano keys as buttons
+    Cs3_key = tk.Button(app, text="C#3", fg="#FFF",image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Cs3)  
+    Cs3_key.place(x=(WHITE_PIANO_KEY_WIDTH*0)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+
+    Ds3_key = tk.Button(app, text="D#3", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Ds3)  
+    Ds3_key.place(x=(WHITE_PIANO_KEY_WIDTH*1)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Fs3_key = tk.Button(app, text="F#3", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Fs3)  
+    Fs3_key.place(x=(WHITE_PIANO_KEY_WIDTH*3)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Gs3_key = tk.Button(app, text="G#3", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Gs3)  
+    Gs3_key.place(x=(WHITE_PIANO_KEY_WIDTH*4)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    As3_key = tk.Button(app, text="A#3", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_As3)  
+    As3_key.place(x=(WHITE_PIANO_KEY_WIDTH*5)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+
+    Cs4_key = tk.Button(app, text="C#4", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Cs4)  
+    Cs4_key.place(x=(WHITE_PIANO_KEY_WIDTH*7)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Ds4_key = tk.Button(app, text="D#4", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Ds4)  
+    Ds4_key.place(x=(WHITE_PIANO_KEY_WIDTH*8)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Fs4_key = tk.Button(app, text="F#4", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Fs4)  
+    Fs4_key.place(x=(WHITE_PIANO_KEY_WIDTH*10)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Gs4_key = tk.Button(app, text="G#4", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Gs4)  
+    Gs4_key.place(x=(WHITE_PIANO_KEY_WIDTH*11)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    As4_key = tk.Button(app, text="A#4", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_As4)  
+    As4_key.place(x=(WHITE_PIANO_KEY_WIDTH*12)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+
+    Cs5_key = tk.Button(app, text="C#5", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Cs5)  
+    Cs5_key.place(x=(WHITE_PIANO_KEY_WIDTH*14)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
+    
+    Ds5_key = tk.Button(app, text="D#5", fg="#FFF", image=size_by_pixel, bg='#000', height=BLACK_PIANO_KEY_HEIGHT, width=BLACK_PIANO_KEY_WIDTH, compound='bottom', anchor="s", command=play_sound.play_Ds5)  
+    Ds5_key.place(x=(WHITE_PIANO_KEY_WIDTH*15)+(WHITE_PIANO_KEY_WIDTH - BLACK_PIANO_KEY_WIDTH/2), y=WINDOW_HEIGHT-WHITE_PIANO_KEY_HEIGHT)
