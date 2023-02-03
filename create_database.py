4a8b0a31be2ce61ed94a15e5b743c7e3014c9555
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-02-03 17:49:22 +0200

    Update database

--- create_database.py
+++ create_database.py
@@ -1,12 +1,33 @@
 import sqlite3
 
-#creating a database 
-database = sqlite3.connect("virtual_piano_and_melody_library.db")
-cursor = database.cursor()
+def create_connection(database_file):
+    return sqlite3.connect(database_file)
 
-cursor.execute("CREATE TABLE IF NOT EXISTS user(username, password, first_name, last_name, role)")
-cursor.execute("CREATE TABLE IF NOT EXISTS melody(name, path, creators_username, visibility)")
+def create_table(database, table):
+    cursor = database.cursor()
+    cursor.execute(table)
+
+database = create_connection("virtual_piano_and_melody_library.db")
+
+user_table = """ CREATE TABLE IF NOT EXISTS users (
+                                    username text PRIMARY KEY,
+                                    password text NOT NULL,
+                                    first_name text NOT NULL,
+                                    last_name text NOT NULL,
+                                    role text NOT NULL
+                                ); """
+
+melodys_table = """CREATE TABLE IF NOT EXISTS melodys (
+                                name text NOT NULL,
+                                path text NOT NULL,
+                                creators_username text NOT NULL,
+                                visibility integer NOT NULL,
+                                PRIMARY KEY (name, creators_username)
+                                FOREIGN KEY (creators_username) REFERENCES users (username)
+                            );"""
+
+create_table(database, user_table)
+create_table(database, melodys_table)
 
 database.commit()
-
-database.close() 
+database.close()
--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def change_to_virtual_piano_page():
     virtual_piano_page.pack(fill='both', expand=1)
     login_frame.pack_forget()
 
+def change_to_registration_page():
+    registration_frame.pack(fill='both', expand=1)
+    login_frame.pack_forget()
+
 app = tk.Tk()
 app.title("Music Library")
 
@@ -21,7 +25,11 @@ app.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 login_frame = tk.Frame(app)
 login_frame.pack()
+registration_frame = tk.Frame(app)
 virtual_piano_page = tk.Frame(app)
+
+create_account_button = tk.Button(login_frame, text='Create account', command=lambda:[change_to_registration_page()])
+create_account_button.pack()
 
 login_button = tk.Button(login_frame, text='Login', command=lambda:[change_to_virtual_piano_page()])
 login_button.pack()
@@ -29,6 +37,8 @@ login_button.pack()
 size_by_pixel = tk.PhotoImage(width=1, height=1)
 key_buttons.create_key_buttons(virtual_piano_page, WINDOW_HEIGHT, size_by_pixel)
 
+
+#record melody 
 record_melody_button = tk.Button(virtual_piano_page, text='Record a melody', command=lambda:[record_melody.record_melody()])
 record_melody_button.pack()
 
