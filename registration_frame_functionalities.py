6fd1abd96ff4798ce70971463ccf1c283a08be22
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-02-03 18:46:03 +0200

    Take input values from registration frame fields

--- registration_frame_functionalities.py
+++ registration_frame_functionalities.py
@@ -1,5 +1,15 @@
 import tkinter as tk
 
+def register(username_input_field, password_input_field, confirm_password_input_field, 
+             first_name_input_field, last_name_input_field, menu):
+    username = username_input_field.get("1.0", "end-1c")
+    password = password_input_field.get("1.0", "end-1c")
+    confirm_password = confirm_password_input_field.get("1.0", "end-1c")
+    first_name = first_name_input_field.get("1.0", "end-1c")
+    last_name = last_name_input_field.get("1.0", "end-1c")
+    role = menu.get()
+
+
 def create_registration_fields(registration_frame):
     tk.Label(registration_frame, text="username:").pack()
     username_input_field = tk.Text(registration_frame, height=1, width=20)
@@ -22,7 +32,12 @@ def create_registration_fields(registration_frame):
     last_name_input_field.pack()
 
     tk.Label(registration_frame, text="role:").pack()
-    menu= tk.StringVar()
+    menu = tk.StringVar()
     menu.set("Select a role")
     role_dropdown = tk.OptionMenu(registration_frame, menu, "User with global rights", "User with local rights")
     role_dropdown.pack()
+
+    register_button = tk.Button(registration_frame, text="Register", 
+                      command=lambda:[register(username_input_field, password_input_field, confirm_password_input_field, 
+                      first_name_input_field, last_name_input_field, menu)])
+    register_button.pack()
