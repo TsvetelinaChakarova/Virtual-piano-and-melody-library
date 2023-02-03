4784493d29044f68d56267a83203e5a3ff932580
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-02-03 12:14:35 +0200

    Add login frame and virtual piano frame

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import record_melody
 WINDOW_WIDTH = 1020
 WINDOW_HEIGHT = 500
 
+def change_to_virtual_piano_page():
+    virtual_piano_page.pack(fill='both', expand=1)
+    login_frame.pack_forget()
+
 app = tk.Tk()
 app.title("Music Library")
 
@@ -15,14 +19,21 @@ app.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
 app.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 app.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-size_by_pixel = tk.PhotoImage(width=1, height=1)
-key_buttons.create_key_buttons(app, WINDOW_HEIGHT, size_by_pixel)
+login_frame = tk.Frame(app)
+login_frame.pack()
+virtual_piano_page = tk.Frame(app)
 
-record_melody_button = tk.Button(text='Record a melody', command=lambda:[record_melody.record_melody()])
+login_button = tk.Button(login_frame, text='Login', command=lambda:[change_to_virtual_piano_page()])
+login_button.pack()
+
+size_by_pixel = tk.PhotoImage(width=1, height=1)
+key_buttons.create_key_buttons(virtual_piano_page, WINDOW_HEIGHT, size_by_pixel)
+
+record_melody_button = tk.Button(virtual_piano_page, text='Record a melody', command=lambda:[record_melody.record_melody()])
 record_melody_button.pack()
 
-stop_record_melody_button = tk.Button(text='Stop recording a melody',
-                                      command=lambda:[record_melody.stop_recording_melody(app)])
+stop_record_melody_button = tk.Button(virtual_piano_page, text='Stop recording a melody',
+                                      command=lambda:[record_melody.stop_recording_melody(virtual_piano_page)])
 stop_record_melody_button.pack()
 
 app.mainloop()
