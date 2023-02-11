f1620939ef0e3b32b809fc9e97191fb3251510c4
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-02-11 23:57:55 +0200

    Add classes for frames

--- login_frame_functionalities.py
+++ login_frame_functionalities.py
@@ -25,20 +25,20 @@ def change_frame_after_login(login_frame, virtual_piano_frame, admin_frame, user
         popup_windows.passwords_error_popup(login_frame, "Invalid username or password!")
 
 def change_to_registration_frame(login_frame, registration_frame):
-    registration_frame.pack(fill='both', expand=1)
+    registration_frame.registration_frame.pack(fill='both', expand=1)
     login_frame.pack_forget()
 
 def create_login_fields(login_frame, registration_frame, virtual_piano_frame, admin_frame):
-    tk.Label(login_frame, text="username:").pack()
-    username_input_field = tk.Entry(login_frame)
+    tk.Label(login_frame.login_frame, text="username:").pack()
+    username_input_field = tk.Entry(login_frame.login_frame)
     username_input_field.pack()
 
-    tk.Label(login_frame, text="password:").pack()
-    password_input_field = tk.Entry(login_frame, show='*')
+    tk.Label(login_frame.login_frame, text="password:").pack()
+    password_input_field = tk.Entry(login_frame.login_frame, show='*')
     password_input_field.pack()
 
-    create_account_button = tk.Button(login_frame, text='Create account', command=lambda:[change_to_registration_frame(login_frame, registration_frame)])
+    create_account_button = tk.Button(login_frame.login_frame, text='Create account', command=lambda:[change_to_registration_frame(login_frame.login_frame, registration_frame)])
     create_account_button.pack()
 
-    login_button = tk.Button(login_frame, text='Login', command=lambda:[change_frame_after_login(login_frame, virtual_piano_frame, admin_frame, username_input_field, password_input_field)])
+    login_button = tk.Button(login_frame.login_frame, text='Login', command=lambda:[change_frame_after_login(login_frame.login_frame, virtual_piano_frame.virtual_piano_frame, admin_frame.admin_frame, username_input_field, password_input_field)])
     login_button.pack()
--- main.py
+++ main.py
@@ -8,34 +8,42 @@ import key_buttons
 WINDOW_WIDTH = 1020
 WINDOW_HEIGHT = 500
 
-app = tk.Tk()
-app.title("Music Library")
+class LoginFrame:
+    def __init__(self, app):
+        self.app = app
+        self.login_frame = tk.Frame(self.app)
+        self.login_frame.pack()
 
-#set current, min and max window size
-app.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
-app.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
-app.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+class RegistrationFrame:
+    def __init__(self, app):
+        self.registration_frame = tk.Frame(app)
+        registration_frame_functionalities.create_registration_fields(self.registration_frame)
 
-# create frames 
-login_frame = tk.Frame(app)
-registration_frame = tk.Frame(app)
-virtual_piano_frame = tk.Frame(app)
-admin_frame = tk.Frame(app)
+class VirtualPianoFrame:
+    def __init__(self, app):
+        self.virtual_piano_frame = tk.Frame(app)
+        virtual_piano_frame_functionities.create_virtual_piano_fields(self.virtual_piano_frame)
 
-# for login_frame
-login_frame.pack()
-login_frame_functionalities.create_login_fields(login_frame, registration_frame, virtual_piano_frame, admin_frame)
+class AdminFrame:
+    def __init__(self, app):
+        self.admin_frame = tk.Frame(app)
+        admin_frame_functionalities.create_admin_fields(self.admin_frame)
 
-# for registration frame
-registration_frame_functionalities.create_registration_fields(registration_frame)
+if __name__ == '__main__':
+    app = tk.Tk()
+    app.title("Music Library")
+    app.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
+    app.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+    app.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-# for admin_frame
-admin_frame_functionalities.create_admin_fields(admin_frame)
+    login_frame = LoginFrame(app)
+    registration_frame = RegistrationFrame(app)
+    virtual_piano_frame = VirtualPianoFrame(app)
+    admin_frame = AdminFrame(app)
+    
+    size_by_pixel = tk.PhotoImage(width=1, height=1)
+    key_buttons.create_key_buttons(virtual_piano_frame.virtual_piano_frame, WINDOW_HEIGHT, size_by_pixel)
 
-# for virtual_piano_frame
-size_by_pixel = tk.PhotoImage(width=1, height=1)
-key_buttons.create_key_buttons(virtual_piano_frame, WINDOW_HEIGHT, size_by_pixel)
+    login_frame_functionalities.create_login_fields(login_frame, registration_frame, virtual_piano_frame, admin_frame)
 
-virtual_piano_frame_functionities.create_virtual_piano_fields(virtual_piano_frame)
-
-app.mainloop()
+    app.mainloop()
