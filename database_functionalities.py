79a3b5bd70c0fcba75efa85b52d007cbac2f77cb
Tsvetelina Chakarova <cvetelinachakurova@gmail.com>
2023-02-16 02:12:22 +0200

    Update database file

--- database_functionalities.py
+++ database_functionalities.py
@@ -8,6 +8,49 @@ def create_table(database, table):
     cursor = database.cursor()
     cursor.execute(table)
 
+database = create_connection("virtual_piano_and_melody_library.db")
+
+user_table = """ CREATE TABLE IF NOT EXISTS users (
+                                    username text PRIMARY KEY,
+                                    password text NOT NULL,
+                                    first_name text NOT NULL,
+                                    last_name text NOT NULL,
+                                    role text NOT NULL
+                                ); """
+
+global_user_additional_info_table = """CREATE TABLE IF NOT EXISTS global_user_additional_info_table (
+                                username text PRIMARY KEY,
+                                motivation text NOT NULL, 
+                                email text NOT NULL,
+                                FOREIGN KEY (username) REFERENCES users (username)
+                            );"""
+
+requests_for_global_user_table = """CREATE TABLE IF NOT EXISTS requests_for_global_user_table (
+                                username text PRIMARY KEY,
+                                password text NOT NULL,
+                                first_name text NOT NULL,
+                                last_name text NOT NULL,
+                                motivation text NOT NULL, 
+                                email text NOT NULL
+                            ); """
+
+melodys_table = """CREATE TABLE IF NOT EXISTS melodys (
+                                name text NOT NULL,
+                                path text NOT NULL,
+                                keywords text NOT NULL,
+                                creators_username text NOT NULL,
+                                visibility integer NOT NULL,
+                                PRIMARY KEY (name, creators_username),
+                                FOREIGN KEY (creators_username) REFERENCES users (username)
+                            );"""
+
+create_table(database, user_table)
+create_table(database, global_user_additional_info_table)
+create_table(database, requests_for_global_user_table)
+create_table(database, melodys_table)
+
+database.commit()
+
 def insert_into_user_table(username, password, first_name, last_name, role):
     insert_query = """INSERT INTO users
                           (username, password, first_name, last_name, role) 
@@ -61,46 +104,3 @@ def get_role_from_username(username):
     for row in records:
         role_for_user = (row[4])
     return role_for_user
-
-database = create_connection("virtual_piano_and_melody_library.db")
-
-user_table = """ CREATE TABLE IF NOT EXISTS users (
-                                    username text PRIMARY KEY,
-                                    password text NOT NULL,
-                                    first_name text NOT NULL,
-                                    last_name text NOT NULL,
-                                    role text NOT NULL
-                                ); """
-
-global_user_additional_info_table = """CREATE TABLE IF NOT EXISTS global_user_additional_info_table (
-                                username text PRIMARY KEY,
-                                motivation text NOT NULL, 
-                                email text NOT NULL,
-                                FOREIGN KEY (username) REFERENCES users (username)
-                            );"""
-
-requests_for_global_user_table = """CREATE TABLE IF NOT EXISTS requests_for_global_user_table (
-                                username text PRIMARY KEY,
-                                password text NOT NULL,
-                                first_name text NOT NULL,
-                                last_name text NOT NULL,
-                                motivation text NOT NULL, 
-                                email text NOT NULL
-                            ); """
-
-melodys_table = """CREATE TABLE IF NOT EXISTS melodys (
-                                name text NOT NULL,
-                                path text NOT NULL,
-                                keywords text NOT NULL,
-                                creators_username text NOT NULL,
-                                visibility integer NOT NULL,
-                                PRIMARY KEY (name, creators_username),
-                                FOREIGN KEY (creators_username) REFERENCES users (username)
-                            );"""
-
-create_table(database, user_table)
-create_table(database, global_user_additional_info_table)
-create_table(database, requests_for_global_user_table)
-create_table(database, melodys_table)
-
-database.commit()
